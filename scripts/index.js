244ed834eddc9f303a3e010920d39c7c94974fd2
japorongao <jan.aporongao@icloud.com>
2025-09-01 11:18:54 -0500

    stage 4

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,22 @@ const editProfileBtn = document.querySelector(".profile_edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProileCloseBtn = editProfileModal.querySelector(".modal_close-btn");
 
+const newPostBtn = document.querySelector(".profile_new-post-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal_close-btn");
+
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+});
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
 });
