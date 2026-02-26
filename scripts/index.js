1999674c84fa4301e7534658cbf2cb8e73248d92
japorongao <jan.aporongao@icloud.com>
2026-02-25 23:29:41 -0600

    JS Error Corrections

--- scripts/index.js
+++ scripts/index.js
@@ -65,11 +65,11 @@ function getCardElement(data) {
 }
 
 function openModal(modal) {
-  modal.classlist.add("modal_is-opened");
+  modal.classList.add("modal_is-opened");
 }
 
 function closeModal(modal) {
-  modal.classlist.remove("modal_is-opened");
+  modal.classList.remove("modal_is-opened");
 }
 
 editProfileBtn.addEventListener("click", function () {
